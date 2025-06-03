5c7bdd0df1214ab74e3fefb398a2a50cad1e6cd9
nomenoparty <khronal777@gmail.com>
2025-06-03 17:57:54 +0700

    Mobile UI Design UML: ex1 and ex2

---
+++ app/src/am_hoanghh/issues2/ex1/Ex1Activity.java
@@ -0,0 +1,26 @@
+package issues2.ex1;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.asian.R;
+
+public class Ex1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_ex1);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.rlExercise1), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
