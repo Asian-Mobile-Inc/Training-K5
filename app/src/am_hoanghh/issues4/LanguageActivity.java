b323fbb225603e7059cb9af8381c5ac9672aedb5
hoanghuu1207 <khronal777@gmail.com>
2025-06-12 10:04:32 +0700

    Fix bug language_code isEmpty

--- app/src/am_hoanghh/issues4/LanguageActivity.java
+++ app/src/am_hoanghh/issues4/LanguageActivity.java
@@ -28,6 +28,27 @@ import java.util.List;
 public class LanguageActivity extends AppCompatActivity {
     private ActivityLanguageBinding binding;
     private static final String LANGUAGE_CODE = "LANGUAGE_CODE";
+    private static final String ENGLISH_LANGUAGE_CODE = "en";
+    private static final String SPANISH_LANGUAGE_CODE = "es";
+    private static final String VIETNAMESE_LANGUAGE_CODE = "vi";
+    private static final String FRENCH_LANGUAGE_CODE = "fr";
+    private static final String HINDI_LANGUAGE_CODE = "hi";
+    private static final String GERMAN_LANGUAGE_CODE = "de";
+    private static final String PORTUGUESE_LANGUAGE_CODE = "pt";
+    private static final String ITALIAN_LANGUAGE_CODE = "it";
+    private static final String ARABIC_LANGUAGE_CODE = "ar";
+    private static final String JAPANESE_LANGUAGE_CODE = "ja";
+    private static final String KOREAN_LANGUAGE_CODE = "ko";
+    private static final String SOUTH_AFRIKAANS_LANGUAGE_CODE = "af";
+    private static final String HEBREW_LANGUAGE_CODE = "iw";
+    private static final String HAUSA_LANGUAGE_CODE = "ha";
+    private static final String CHINESE_LANGUAGE_CODE = "zh";
+    private static final String TURKISH_LANGUAGE_CODE = "tr";
+    private static final String BAHASA_INDONESIA_LANGUAGE_CODE = "in";
+    private static final String DUTCH_LANGUAGE_CODE = "nl";
+    private static final String RUSSIAN_LANGUAGE_CODE = "ru";
+    private static final String POLISH_LANGUAGE_CODE = "pl";
+
     private int languagePosition;
 
     @Override
@@ -39,6 +60,10 @@ public class LanguageActivity extends AppCompatActivity {
         // Get selected language position
         SharedPreferences sharedPreferences = getSharedPreferences(getString(R.string.user_preferences_key), MODE_PRIVATE);
         String languageCode = sharedPreferences.getString(LANGUAGE_CODE, "");
+
+        if (languageCode.isEmpty()) {
+            languageCode = ENGLISH_LANGUAGE_CODE;
+        }
 
         // Localization
         Locale locale = new Locale(languageCode);
@@ -95,26 +120,26 @@ public class LanguageActivity extends AppCompatActivity {
     public ArrayList<LanguageItem> getLanguageLists(String languageCode) {
         ArrayList<LanguageItem> languageLists = new ArrayList<>();
 
-        languageLists.add(new LanguageItem(R.drawable.ic_english, getString(R.string.language_item_english), getString(R.string.language_code_english)));
-        languageLists.add(new LanguageItem(R.drawable.ic_spain, getString(R.string.language_item_spainish), getString(R.string.language_code_spainish)));
-        languageLists.add(new LanguageItem(R.drawable.ic_vietnam, getString(R.string.language_item_vietnamese), getString(R.string.language_code_vietnamese)));
-        languageLists.add(new LanguageItem(R.drawable.ic_france, getString(R.string.language_item_french), getString(R.string.language_code_french)));
-        languageLists.add(new LanguageItem(R.drawable.ic_india, getString(R.string.language_item_hindi), getString(R.string.language_code_hindi)));
-        languageLists.add(new LanguageItem(R.drawable.ic_germany, getString(R.string.language_item_german), getString(R.string.language_code_german)));
-        languageLists.add(new LanguageItem(R.drawable.ic_portugal, getString(R.string.language_item_portuguese), getString(R.string.language_code_portuguese)));
-        languageLists.add(new LanguageItem(R.drawable.ic_italy, getString(R.string.language_item_italian), getString(R.string.language_code_italian)));
-        languageLists.add(new LanguageItem(R.drawable.ic_arabic, getString(R.string.language_item_arabic), getString(R.string.language_code_arabic)));
-        languageLists.add(new LanguageItem(R.drawable.ic_japan, getString(R.string.language_item_japanese), getString(R.string.language_code_japanese)));
-        languageLists.add(new LanguageItem(R.drawable.ic_korea, getString(R.string.language_item_korean), getString(R.string.language_code_korean)));
-        languageLists.add(new LanguageItem(R.drawable.ic_south_africa, getString(R.string.language_item_south_afrikaans), getString(R.string.language_code_south_afrikaans)));
-        languageLists.add(new LanguageItem(R.drawable.ic_israel, getString(R.string.language_item_hebrew), getString(R.string.language_code_hebrew)));
-        languageLists.add(new LanguageItem(R.drawable.ic_nigeria, getString(R.string.language_item_hausa), getString(R.string.language_code_hausa)));
-        languageLists.add(new LanguageItem(R.drawable.ic_china, getString(R.string.language_item_chinese), getString(R.string.language_code_chinese)));
-        languageLists.add(new LanguageItem(R.drawable.ic_turkey, getString(R.string.language_item_turkish), getString(R.string.language_code_turkish)));
-        languageLists.add(new LanguageItem(R.drawable.ic_indonesia, getString(R.string.language_item_bahasa_indonesia), getString(R.string.language_code_bahasa_indonesia)));
-        languageLists.add(new LanguageItem(R.drawable.ic_netherlands, getString(R.string.language_item_dutch), getString(R.string.language_code_dutch)));
-        languageLists.add(new LanguageItem(R.drawable.ic_russia, getString(R.string.language_item_russian), getString(R.string.language_code_russian)));
-        languageLists.add(new LanguageItem(R.drawable.ic_poland, getString(R.string.language_item_polish), getString(R.string.language_code_polish)));
+        languageLists.add(new LanguageItem(R.drawable.ic_english, getString(R.string.language_item_english), ENGLISH_LANGUAGE_CODE));
+        languageLists.add(new LanguageItem(R.drawable.ic_spain, getString(R.string.language_item_spainish), SPANISH_LANGUAGE_CODE));
+        languageLists.add(new LanguageItem(R.drawable.ic_vietnam, getString(R.string.language_item_vietnamese), VIETNAMESE_LANGUAGE_CODE));
+        languageLists.add(new LanguageItem(R.drawable.ic_france, getString(R.string.language_item_french), FRENCH_LANGUAGE_CODE));
+        languageLists.add(new LanguageItem(R.drawable.ic_india, getString(R.string.language_item_hindi), HINDI_LANGUAGE_CODE));
+        languageLists.add(new LanguageItem(R.drawable.ic_germany, getString(R.string.language_item_german), GERMAN_LANGUAGE_CODE));
+        languageLists.add(new LanguageItem(R.drawable.ic_portugal, getString(R.string.language_item_portuguese), PORTUGUESE_LANGUAGE_CODE));
+        languageLists.add(new LanguageItem(R.drawable.ic_italy, getString(R.string.language_item_italian), ITALIAN_LANGUAGE_CODE));
+        languageLists.add(new LanguageItem(R.drawable.ic_arabic, getString(R.string.language_item_arabic), ARABIC_LANGUAGE_CODE));
+        languageLists.add(new LanguageItem(R.drawable.ic_japan, getString(R.string.language_item_japanese), JAPANESE_LANGUAGE_CODE));
+        languageLists.add(new LanguageItem(R.drawable.ic_korea, getString(R.string.language_item_korean), KOREAN_LANGUAGE_CODE));
+        languageLists.add(new LanguageItem(R.drawable.ic_south_africa, getString(R.string.language_item_south_afrikaans), SOUTH_AFRIKAANS_LANGUAGE_CODE));
+        languageLists.add(new LanguageItem(R.drawable.ic_israel, getString(R.string.language_item_hebrew), HEBREW_LANGUAGE_CODE));
+        languageLists.add(new LanguageItem(R.drawable.ic_nigeria, getString(R.string.language_item_hausa), HAUSA_LANGUAGE_CODE));
+        languageLists.add(new LanguageItem(R.drawable.ic_china, getString(R.string.language_item_chinese), CHINESE_LANGUAGE_CODE));
+        languageLists.add(new LanguageItem(R.drawable.ic_turkey, getString(R.string.language_item_turkish), TURKISH_LANGUAGE_CODE));
+        languageLists.add(new LanguageItem(R.drawable.ic_indonesia, getString(R.string.language_item_bahasa_indonesia), BAHASA_INDONESIA_LANGUAGE_CODE));
+        languageLists.add(new LanguageItem(R.drawable.ic_netherlands, getString(R.string.language_item_dutch), DUTCH_LANGUAGE_CODE));
+        languageLists.add(new LanguageItem(R.drawable.ic_russia, getString(R.string.language_item_russian), RUSSIAN_LANGUAGE_CODE));
+        languageLists.add(new LanguageItem(R.drawable.ic_poland, getString(R.string.language_item_polish), POLISH_LANGUAGE_CODE));
 
         for (int i = 0; i < languageLists.size(); i++) {
             if (languageLists.get(i).getLanguageCode().equals(languageCode)) {
