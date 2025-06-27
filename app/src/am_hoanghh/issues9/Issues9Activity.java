8d6d1b873ad299667a16901c3a2a3e0ab71589c9
hoanghuu1207 <khronal777@gmail.com>
2025-06-27 15:56:44 +0700

    feat(custom view): TODO don't render view is out of width

--- app/src/am_hoanghh/issues9/Issues9Activity.java
+++ app/src/am_hoanghh/issues9/Issues9Activity.java
@@ -1,30 +1,70 @@
 package issues9;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+import com.example.asian.R;
 import com.example.asian.databinding.ActivityIssues9Binding;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Issues9Activity extends AppCompatActivity {
-    private ActivityIssues9Binding binding;
-    private static final List<Double> DATA_RAINS = Arrays.asList(8.0, 7.0, 8.0, 0.0, 5.0, 2.0, 16.2, 8.1, 9.2, 7.2, 9.3, 10.5, 12.6, 8.7, 8.2, 6.3);
-    private static final List<Double> DATA_WINTRYS = Arrays.asList(2.0, 0.0, 3.0, 6.0, 10.0, 2.0, 3.2, 8.0, 1.2, 6.8, 2.0, 4.5, 0.0, 9., 1.2, 3.5);
+    private ActivityIssues9Binding mBinding;
+    private List<Hour> mHourLists;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityIssues9Binding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        mBinding = ActivityIssues9Binding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
 
-        int[] graphArray = {8, 7, 8, 2, 5, 2, 18, 8, 9, 7, 9, 10, 12, 8, 8, 6, 8, 9, 11, 7, 4, 5, 8, 10};
+        initHourLists();
+        Weather weather = new Weather(this, mHourLists, mOnTouchChartListener);
+        mBinding.rlWeather.addView(weather);
+    }
 
-        List<Hour> hourLists = Arrays.asList(
+    private final OnTouchChartListener mOnTouchChartListener = new OnTouchChartListener() {
+        @Override
+        public void onTouchListener(int selectedIndex) {
+            if (selectedIndex >= 0) {
+                Hour item = mHourLists.get(selectedIndex);
+                mBinding.tvRainFall.setText(getString(R.string.textview_text_number_mm, item.getmRain()));
+
+                // Draw status weather icon
+                int resourceId = 0;
+                if (item.getmRain() > 0) {
+                    resourceId = R.drawable.ic_rain_big;
+                } else if (item.getmWintry() > 0) {
+                    resourceId = R.drawable.ic_cloud_big;
+                } else {
+                    resourceId = R.drawable.ic_sun_big;
+                }
+                mBinding.ivStatusWeather.setVisibility(View.VISIBLE);
+                Glide.with(getApplicationContext())
+                        .load(resourceId)
+                        .override(34, 34)
+                        .into(mBinding.ivStatusWeather);
+
+                mBinding.tvTemperatureHour.setText(getString(R.string.textview_text_number_temperature, item.getmTemperature()));
+                mBinding.tvDescription.setText("");
+                mBinding.tvHour.setText(getString(R.string.textview_text_hour, selectedIndex));
+            } else {
+                mBinding.tvRainFall.setText("");
+                mBinding.ivStatusWeather.setVisibility(View.GONE);
+                mBinding.tvTemperatureHour.setText("");
+                mBinding.tvDescription.setText(getString(R.string.textview_text_total_in_the_past_24_hours));
+                mBinding.tvHour.setText("");
+            }
+        }
+    };
+
+    private void initHourLists() {
+        mHourLists = Arrays.asList(
                 new Hour(2f, 0.5f, 0, 22),
                 new Hour(0, 0, 0, 23),
                 new Hour(0, 0, 0, 21),
@@ -40,7 +80,7 @@ public class Issues9Activity extends AppCompatActivity {
                 new Hour(8.5f, 6, 0, 23),
                 new Hour(2, 1, 0, 24),
                 new Hour(0, 0, 0, 23.5f),
-                new Hour(0, 0, 0, 22),
+                new Hour(0, 0.5f, 0, 22),
                 new Hour(0, 0, 0, 22.5f),
                 new Hour(0, 2.5f, 0, 23),
                 new Hour(3.5f, 6, 0, 22.5f),
@@ -50,10 +90,5 @@ public class Issues9Activity extends AppCompatActivity {
                 new Hour(5, 0, 0, 21),
                 new Hour(10, 7, 0, 20)
         );
-
-        Weather weather = new Weather(this, hourLists);
-        binding.rlWeather.addView(weather);
-
-
     }
 }
--- app/src/am_hoanghh/issues9/Weather.java
+++ app/src/am_hoanghh/issues9/Weather.java
@@ -2,9 +2,17 @@ package issues9;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.DashPathEffect;
+import android.graphics.LinearGradient;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.text.TextPaint;
 import android.util.DisplayMetrics;
 import android.util.Log;
@@ -14,73 +22,100 @@ import android.view.ScaleGestureDetector;
 import android.view.View;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.res.ResourcesCompat;
 
 import com.example.asian.R;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Weather extends View {
+    private final Context mContext;
     private Paint mTempPaint;
     private Paint mDottedLinePaint;
     private Paint mSolidLinePaint;
     private TextPaint mTextHourPaint;
     private TextPaint mTextLevelRainPaint;
+    private Paint mSliderPaint;
+    private Paint mRainPaint;
+    private Paint mWintryPaint;
+    private Paint mPolygonPaint;
+    private Paint mStatusWeatherPaint;
 
-    private ScaleGestureDetector scaleGestureDetector;
+    private final List<Hour> mHourLists;
+    private final List<Point> mPointLists = new ArrayList<>();
+    private final OnTouchChartListener mListener;
+
+    private ScaleGestureDetector mScaleGestureDetector;
     private float lastTouchX;
     private boolean isDragging = false;
     private float translateX = 0f;
     private float scaleFactor = 1.0f;
 
-    private Paint mRainPaint;
-    private Paint mWintryPaint;
-    private List<Hour> mHourLists;
-
     private static int sLevelRain;
+    private static float sMaxXValue;
+    private static boolean sIsActionDown;
+    private static float sXSliderPosition;
 
-    public Weather(Context context, List<Hour> hourLists) {
+    private static final float BAR_WIDTH = 6.0f;
+    private static final float MARGIN_CHART_TO_RIGHT = 25.0f;
+    private static final float MARGIN_CHART_TO_TOP = 30.0f;
+    private static final float MARGIN_CHART_TO_BOTTOM = 30.0f;
+
+    public Weather(Context context, List<Hour> hourLists, OnTouchChartListener listener) {
         super(context);
-        init(context);
-        init1();
-
         this.mHourLists = hourLists;
+        this.mContext = context;
+        this.mListener = listener;
+        init();
+        initScaleGestureDetector();
     }
 
-    private void init(Context context) {
+    private void init() {
         mTempPaint = new Paint(Paint.ANTI_ALIAS_FLAG | Paint.LINEAR_TEXT_FLAG);
-        mTempPaint.setColor(context.getColor(R.color.yellow_FFAD2A));
+        mTempPaint.setColor(mContext.getColor(R.color.yellow_FFAD2A));
         mTempPaint.setStrokeWidth(dpToFloat(3));
         mTempPaint.setStrokeCap(Paint.Cap.ROUND);
 
         mDottedLinePaint = new Paint();
-        mDottedLinePaint.setColor(context.getColor(R.color.white_15FFFFFF));
+        mDottedLinePaint.setColor(mContext.getColor(R.color.white_15FFFFFF));
         mDottedLinePaint.setStyle(Paint.Style.STROKE);
-        mDottedLinePaint.setPathEffect(new DashPathEffect(new float[] {3f,3f}, 0f));
+        mDottedLinePaint.setPathEffect(new DashPathEffect(new float[]{3f, 3f}, 0f));
 
         mSolidLinePaint = new Paint();
-        mSolidLinePaint.setColor(context.getColor(R.color.white_15FFFFFF));
+        mSolidLinePaint.setColor(mContext.getColor(R.color.white_15FFFFFF));
         mSolidLinePaint.setStyle(Paint.Style.STROKE);
 
         mTextHourPaint = new TextPaint();
-        mTextHourPaint.setColor(context.getColor(R.color.white_40FFFFFF));
+        mTextHourPaint.setColor(mContext.getColor(R.color.white_40FFFFFF));
         mTextHourPaint.setTextSize(dpToFloat(12));
 
         mTextLevelRainPaint = new TextPaint();
-        mTextLevelRainPaint.setColor(context.getColor(R.color.white_96FFFFFF));
+        mTextLevelRainPaint.setColor(mContext.getColor(R.color.white_96FFFFFF));
         mTextLevelRainPaint.setTextSize(dpToFloat(8));
 
         mRainPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mRainPaint.setColor(context.getColor(R.color.blue_81CFFA));
+        mRainPaint.setColor(mContext.getColor(R.color.blue_81CFFA));
         mRainPaint.setStyle(Paint.Style.FILL);
         mRainPaint.setStrokeCap(Paint.Cap.ROUND);
 
         mWintryPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mWintryPaint.setColor(context.getColor(R.color.purple_8185FA));
+        mWintryPaint.setColor(mContext.getColor(R.color.purple_8185FA));
         mWintryPaint.setStyle(Paint.Style.FILL);
         mWintryPaint.setStrokeCap(Paint.Cap.ROUND);
+
+        mSliderPaint = new Paint();
+        mSliderPaint.setColor(mContext.getColor(R.color.white));
+        mSliderPaint.setStyle(Paint.Style.STROKE);
+        mSliderPaint.setStrokeWidth(dpToFloat(0.75f));
+
+        mPolygonPaint = new Paint();
+        mPolygonPaint.setStyle(Paint.Style.FILL);
+
+        mStatusWeatherPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
     }
 
-    public float dpToFloat(int dpSize) {
+    public float dpToFloat(float dpSize) {
         DisplayMetrics dm = getResources().getDisplayMetrics();
         return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dpSize, dm);
     }
@@ -88,17 +123,25 @@ public class Weather extends View {
     @Override
     protected void onDraw(@NonNull Canvas canvas) {
         super.onDraw(canvas);
-
         canvas.save();
-        canvas.scale(scaleFactor, 1.0f);
         canvas.translate(translateX, 0);
+        canvas.scale(scaleFactor, 1.0f);
 
         drawColumnChart(canvas);
         drawTemperature(canvas);
+        drawPolygonGradient(canvas);
         drawDottedLine(canvas);
         drawSolidLine(canvas);
+        drawSlider(canvas);
 
         canvas.restore();
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        LinearGradient gradient = new LinearGradient(0, 0, 0, h, mContext.getColor(R.color.yellow_7AC8A534), mContext.getColor(R.color.yellow_00C8A534), Shader.TileMode.CLAMP);
+        mPolygonPaint.setShader(gradient);
     }
 
     private void drawTemperature(Canvas canvas) {
@@ -108,6 +151,9 @@ public class Weather extends View {
         float factorX = (getWidth() - dpToFloat(25)) / (float) maxX;
         float factorY = (getHeight() - dpToFloat(60)) / maxY;
 
+        // To draw polygon gradient
+        mPointLists.clear();
+        mPointLists.add(new Point(0, getHeight() - dpToFloat(30)));
         for (int i = 1; i < mHourLists.size(); ++i) {
             int x0 = i - 1;
             float y0 = mHourLists.get(i - 1).getmTemperature();
@@ -118,13 +164,32 @@ public class Weather extends View {
             int ex = (int) (i * factorX);
             int ey = getHeight() - (int) dpToFloat(30) - (int) (y1 * factorY);
             canvas.drawLine(sx, sy * 2, ex, ey * 2, mTempPaint);
+
+            if (i == 1) {
+                mPointLists.add(new Point(sx, sy * 2));
+            }
+            mPointLists.add(new Point(ex, ey * 2));
+
+            if (i == mHourLists.size() - 1) {
+                sMaxXValue = ex;
+                mPointLists.add(new Point(ex, getHeight() - dpToFloat(30)));
+            }
         }
+        mPointLists.add(new Point(0, getHeight() - dpToFloat(30)));
     }
 
-    @SuppressLint("DefaultLocale")
+    private void drawPolygonGradient(Canvas canvas) {
+        Path path = new Path();
+        path.reset();
+        path.moveTo(mPointLists.get(0).x, mPointLists.get(0).y);
+        for (int i = 1; i < mPointLists.size(); i++) {
+            path.lineTo(mPointLists.get(i).x, mPointLists.get(i).y);
+        }
+        canvas.drawPath(path, mPolygonPaint);
+    }
+
     private void drawColumnChart(Canvas canvas) {
         float barWidth = dpToFloat(6);
-
         int maxX = 15;
         float maxY = getMaxValueColumnChart();
         float factorX = (getWidth() - dpToFloat(41)) / (float) maxX;
@@ -132,16 +197,26 @@ public class Weather extends View {
         sLevelRain = (int) maxY / 5;
 
         for (int i = 0; i < mHourLists.size(); i++) {
+            // Draw rain column
             float xR = i * factorX + dpToFloat(4);
             float yR = (float) (getHeight() - dpToFloat(30) - mHourLists.get(i).getmRain() * factorY);
             canvas.drawRoundRect(xR, yR, xR + barWidth, getHeight() - dpToFloat(30), 70f, 30f, mRainPaint);
 
+            // Draw wintry column
             float yW = (float) (getHeight() - dpToFloat(30) - mHourLists.get(i).getmWintry() * factorY);
-            canvas.drawRoundRect(xR + dpToFloat(6), yW, xR + dpToFloat(6) + barWidth, getHeight() - dpToFloat(30), 70f, 30f, mWintryPaint);
+            canvas.drawRoundRect(xR + barWidth, yW, xR + barWidth * 2, getHeight() - dpToFloat(30), 70f, 30f, mWintryPaint);
 
+            // Draw hour text
             if (i % 4 == 0) {
-                canvas.drawText(String.format("%02d", i), xR - dpToFloat(1), getHeight() - dpToFloat(16), mTextHourPaint);
+                canvas.drawText(mContext.getString(R.string.drawtext_text_hour, i), xR - dpToFloat(1), getHeight() - dpToFloat(16), mTextHourPaint);
             }
+
+            // Draw icon weather bitmap
+            Bitmap bitmap = getBitmapFromStatusWeather(mHourLists.get(i));
+            Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+            float top = dpToFloat(12);
+            RectF rectF = new RectF(xR, top, xR + bitmap.getWidth(), top + bitmap.getHeight());
+            canvas.drawBitmap(bitmap, rect, rectF, mStatusWeatherPaint);
         }
     }
 
@@ -160,24 +235,29 @@ public class Weather extends View {
         canvas.drawLine(factorX * 4, y, factorX * 4, 0, mSolidLinePaint);
 
         float factorY = (float) (getHeight() - dpToFloat(60)) / 5;
-//        canvas.drawLine(0, (float) getHeight() - dpToFloat(30), factorX * 4, (float) getHeight() - dpToFloat(30), mSolidLinePaint);
         for (int i = 0; i < 6; i++) {
             float xPoint = (float) (getWidth() - dpToFloat(25));
             float yPoint = dpToFloat(30) + factorY * i;
             canvas.drawLine(0, yPoint, xPoint, yPoint, mSolidLinePaint);
 
+            // Draw level rain text
             if (i > 0) {
                 canvas.drawText(i == 5 ? "0 mm" : String.valueOf(sLevelRain * (5 - i)), xPoint + dpToFloat(3), yPoint + dpToFloat(3), mTextLevelRainPaint);
             }
         }
     }
 
-    private void init1() {
-        scaleGestureDetector = new ScaleGestureDetector(getContext(), new ScaleGestureDetector.SimpleOnScaleGestureListener() {
+    private void initScaleGestureDetector() {
+        mScaleGestureDetector = new ScaleGestureDetector(getContext(), new ScaleGestureDetector.SimpleOnScaleGestureListener() {
             @Override
             public boolean onScale(@NonNull ScaleGestureDetector detector) {
-                scaleFactor *= detector.getScaleFactor();
-                scaleFactor = Math.max(0.5f, Math.min(scaleFactor, 3.0f));
+                float proposedScale = scaleFactor * detector.getScaleFactor();
+                float newContentWidth = sMaxXValue * proposedScale;
+                if (newContentWidth < getWidth()) {
+                    proposedScale = getWidth() / sMaxXValue;
+                }
+                proposedScale = Math.max(0.5f, Math.min(proposedScale, 2.0f));
+                scaleFactor = proposedScale;
                 invalidate();
                 return true;
             }
@@ -186,19 +266,30 @@ public class Weather extends View {
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        scaleGestureDetector.onTouchEvent(event);
+        mScaleGestureDetector.onTouchEvent(event);
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 lastTouchX = event.getX();
                 isDragging = true;
+                sIsActionDown = true;
 
-//                Log.d("DEBUG", String.valueOf(event.getX()));
-//                Log.d("DEBUG", String.valueOf(event.getY()));
+                float clickXInChart = (event.getX() - translateX) / scaleFactor;
+                int maxX = 15;
+                float factorX = (getWidth() - dpToFloat(41)) / (float) maxX;
+
+                int selectedIndex = Math.round((clickXInChart - dpToFloat(6)) / factorX);
+                if (selectedIndex < 0) selectedIndex = 0;
+                if (selectedIndex >= mHourLists.size()) selectedIndex = mHourLists.size() - 1;
+
+                mListener.onTouchListener(selectedIndex);
+                sXSliderPosition = selectedIndex * factorX + dpToFloat(10);
+                invalidate();
                 break;
             case MotionEvent.ACTION_MOVE:
                 if (isDragging) {
                     float dx = event.getX() - lastTouchX;
                     translateX += dx;
+                    checkContentOutOfWidth();
                     lastTouchX = event.getX();
                     invalidate();
                 }
@@ -206,6 +297,9 @@ public class Weather extends View {
             case MotionEvent.ACTION_UP:
             case MotionEvent.ACTION_CANCEL:
                 isDragging = false;
+                sIsActionDown = false;
+                mListener.onTouchListener(-1);
+                invalidate();
                 break;
         }
         return true;
@@ -213,7 +307,7 @@ public class Weather extends View {
 
     private Float getMaxValueColumnChart() {
         float max = 0;
-        for (Hour hour: mHourLists) {
+        for (Hour hour : mHourLists) {
             max = Math.max(max, Math.max(hour.getmRain(), hour.getmWintry()));
         }
         return max;
@@ -221,9 +315,63 @@ public class Weather extends View {
 
     private Float getMaxValueTemperature() {
         float max = 0;
-        for (Hour hour: mHourLists) {
+        for (Hour hour : mHourLists) {
             max = Math.max(max, hour.getmTemperature());
         }
         return max;
+    }
+
+    private void drawSlider(Canvas canvas) {
+        if (sIsActionDown) {
+            float y = (float) getHeight() - dpToFloat(10);
+            canvas.drawLine(sXSliderPosition, 0, sXSliderPosition, y, mSliderPaint);
+        }
+    }
+
+    private void checkContentOutOfWidth() {
+        float contentWidth = sMaxXValue * scaleFactor;
+        float maxTranslateX = getWidth() - contentWidth;
+
+        if (contentWidth <= getWidth()) {
+            translateX = 0;
+        } else {
+            if (translateX > 0) {
+                translateX = 0;
+            } else if (translateX < maxTranslateX) {
+                translateX = maxTranslateX;
+            }
+        }
+    }
+
+    private Bitmap drawableToBitmap(Drawable drawable) {
+        if (drawable instanceof BitmapDrawable) {
+            return ((BitmapDrawable) drawable).getBitmap();
+        }
+
+        int width = drawable.getIntrinsicWidth();
+        width = width > 0 ? width : 1;
+        int height = drawable.getIntrinsicHeight();
+        height = height > 0 ? height : 1;
+
+        Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+
+        return bitmap;
+    }
+
+    private Bitmap getBitmapFromStatusWeather(Hour item) {
+        int resourceId = 0;
+        if (item.getmRain() > 0) {
+            resourceId = R.drawable.ic_rain_top;
+        } else if (item.getmWintry() > 0) {
+            resourceId = R.drawable.ic_cloud;
+        } else {
+            resourceId = R.drawable.ic_sun;
+        }
+
+        Drawable drawable = ResourcesCompat.getDrawable(getResources(), resourceId, null);
+        return drawableToBitmap(drawable);
     }
 }
