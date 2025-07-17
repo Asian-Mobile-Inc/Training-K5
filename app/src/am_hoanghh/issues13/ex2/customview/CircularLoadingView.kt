179b8094601f0617900295a17bb5f59d31864d1b
hoanghuu1207 <khronal777@gmail.com>
2025-07-17 11:27:19 +0700

    feat(progressBar): update progressBar using customview

---
+++ app/src/am_hoanghh/issues13/ex2/customview/CircularLoadingView.kt
@@ -0,0 +1,72 @@
+package issues13.ex2.customview
+
+import android.animation.ValueAnimator
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.View
+import android.view.animation.LinearInterpolator
+import com.example.asian.R
+
+class CircularLoadingView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyle: Int = 0
+) : View(context, attrs, defStyle) {
+
+    private val strokeWidth = 16f
+    private val sweepAngle = 45f
+    private var rotateAngle = 0f
+    private val duration = 1000L
+
+    private val bgPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        color = context.getColor(R.color.blue_B3BDFF)
+        style = Paint.Style.STROKE
+        strokeWidth = this@CircularLoadingView.strokeWidth
+    }
+
+    private val fgPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        color = context.getColor(R.color.purple_576CEC)
+        style = Paint.Style.STROKE
+        strokeWidth = this@CircularLoadingView.strokeWidth
+        strokeCap = Paint.Cap.ROUND
+    }
+
+    private val animator = ValueAnimator.ofFloat(0f, 360f).apply {
+        duration = this@CircularLoadingView.duration
+        interpolator = LinearInterpolator()
+        repeatCount = ValueAnimator.INFINITE
+        addUpdateListener {
+            rotateAngle = it.animatedValue as Float
+            invalidate()
+        }
+    }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        animator.start()
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        animator.cancel()
+    }
+
+    @SuppressLint("DrawAllocation")
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        val radius = (width.coerceAtMost(height) - strokeWidth) / 2f
+        val cx = width / 2f
+        val cy = height / 2f
+        val rect = RectF(
+            cx - radius,
+            cy - radius,
+            cx + radius,
+            cy + radius
+        )
+
+        canvas.drawArc(rect, 0f, 360f, false, bgPaint)
+        canvas.drawArc(rect, rotateAngle, sweepAngle, false, fgPaint)
+    }
+}
